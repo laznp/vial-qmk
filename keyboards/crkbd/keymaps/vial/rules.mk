0149766ee3da8d8f6b53937134f457212036d634
Lazuardi N Putra <me@laznp.id>
2024-11-12 00:16:40 +0700

    vial update

--- keyboards/crkbd/keymaps/vial/rules.mk
+++ keyboards/crkbd/keymaps/vial/rules.mk
@@ -5,9 +5,9 @@ LTO_ENABLE          = yes
 RGBLIGHT_ENABLE     = no
 RGB_MATRIX_ENABLE   = no # Can't have RGBLIGHT and RGB_MATRIX at the same time.
 MOUSEKEY_ENABLE     = no
-OLED_ENABLE         = yes
+OLED_ENABLE         = no
 OLED_DRIVER         = ssd1306
-EXTRAKEY_ENABLE     = no
-COMBO_ENABLE        = no
+EXTRAKEY_ENABLE     = yes
+COMBO_ENABLE        = yes
 
 QMK_SETTINGS        = no
